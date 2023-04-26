8970786a7980894499391c9c871998e37de22277
nevermayer <jrgamespro@gmail.com>
2023-04-26 13:46:22 -0400

    Update fibo.py

--- p4/fibo.py
+++ p4/fibo.py
@@ -1,15 +1,15 @@
-import multiprocessing
+from multiprocessing import Pool
 
 def fibonacci_formula(n):
     golden_ratio = (1 + 5 ** 0.5) / 2
     fib_n = int((golden_ratio ** n - (1 - golden_ratio) ** n) / 5 ** 0.5)
     return fib_n
 
-def fibonacci_multiprocessing():
-    with multiprocessing.Pool() as pool:
-        results = pool.map(fibonacci_formula, range(1001))
-    return results
 
 if __name__ == '__main__':
-    print(fibonacci_multiprocessing())
-
+    n = 1000
+    num_procesadores = 4
+    pool = Pool(processes=num_procesadores)
+    results = pool.map(fibonacci_formula, range(n+1))
+    for data in results:
+      print(data)
